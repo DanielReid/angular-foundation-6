604967fd2d657771c42f2e8f172ac6969738e581
Daan Reid <osmosisch@gmail.com>
2017-10-31 09:57:40 +0100

    dist

--- dist/angular-foundation-no-tpls.js
+++ dist/angular-foundation-no-tpls.js
@@ -70,7 +70,7 @@
      * angular-foundation-6
      * http://circlingthesun.github.io/angular-foundation-6/
     
-     * Version: 0.11.11 - 2017-10-03
+     * Version: 0.11.11 - 2017-10-31
      * License: MIT
      * (c) 
      */
@@ -2834,7 +2834,6 @@
                         var tooltipLinker = $compile(template);
 
                         return function link(scope, element, attrs) {
-                            var $body = angular.element(document.querySelector('body'));
                             var tooltip;
                             var popupTimeout;
                             var appendToBody = angular.isDefined(options.appendToBody) ? options.appendToBody : false;
@@ -2853,12 +2852,6 @@
                                 ttWidth = tooltip.prop('offsetWidth');
                                 ttHeight = tooltip.prop('offsetHeight');
 
-                                var scrollTop = $window.pageYOffset;
-                                if (scope.tt_placement === 'top' && position.top - scrollTop - ttHeight - 20 < 0) {
-                                    scope.tt_placement = 'bottom';
-                                }
-
-                                var tt_remSize = parseFloat(getComputedStyle(tooltip[0]).fontSize);
                                 // Calculate the tooltip's top and left coordinates to center it with
                                 // this directive.
                                 switch (scope.tt_placement) {
@@ -2871,7 +2864,7 @@
                                     case 'bottom':
                                         ttPosition = {
                                             top: position.top + position.height + 10,
-                                            left: position.left - 2.25 * tt_remSize + element[0].offsetWidth / 2
+                                            left: position.left - ttWidth / 2 + position.width / 2
                                         };
                                         break;
                                     case 'left':
@@ -2881,10 +2874,9 @@
                                         };
                                         break;
                                     default:
-                                        //top
                                         ttPosition = {
                                             top: position.top - ttHeight - 10,
-                                            left: position.left - 2.25 * tt_remSize + element[0].offsetWidth / 2
+                                            left: position.left - ttWidth / 2 + position.width / 2
                                         };
                                         break;
                                 }
@@ -2920,23 +2912,6 @@
                                     }, angular.noop);
                                 } else {
                                     show()();
-                                }
-                            }
-
-                            function closeOnClick(e) {
-                                var elementContents = Array.prototype.slice.apply(element[0].querySelectorAll('*'));
-
-                                if (!elementContents.length) {
-                                    return;
-                                }
-
-                                var isOuterElement = elementContents.every(function (node) {
-                                    return node !== e.target;
-                                });
-
-                                if (isOuterElement) {
-                                    hide();
-                                    scope.$apply();
                                 }
                             }
 
@@ -2979,8 +2954,6 @@
                                 scope.tt_isOpen = true;
                                 scope.$digest(); // digest required as $apply is not called
 
-                                $body.on('click', closeOnClick);
-
                                 // Return positioning function as promise callback for correct
                                 // positioning after draw.
                                 return positionTooltip;
@@ -2993,7 +2966,6 @@
 
                                 //if tooltip is going to be shown after delay, we must cancel this
                                 $timeout.cancel(popupTimeout);
-                                $body.off('click', closeOnClick);
                                 removeTooltip();
                             }
 
