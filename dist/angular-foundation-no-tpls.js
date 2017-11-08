6ddb6a90f17a7263020cea5ef3b9bc14be84a569
Daan Reid <osmosisch@gmail.com>
2017-11-08 14:49:51 +0100

    chore(tooltip): build artefacts

--- dist/angular-foundation-no-tpls.js
+++ dist/angular-foundation-no-tpls.js
@@ -70,7 +70,7 @@
      * angular-foundation-6
      * http://circlingthesun.github.io/angular-foundation-6/
     
-     * Version: 0.11.15 - 2017-11-05
+     * Version: 0.11.16 - 2017-11-08
      * License: MIT
      * (c) 
      */
